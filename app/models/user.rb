2404c2f56aafcb169fc7151b67d79fc921fa618a
Robert Cornell <robert.cornell78@gmai.com>
2016-01-19 21:19:08 -0700

    user model, home and session controller built

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+  has_secure_password
+
+  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :password_digest, presence: true
+end
